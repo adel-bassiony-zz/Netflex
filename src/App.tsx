71acf8020d7a9199d7ef919dc15149cba3fc62a8
Adel Bassiony <a.bassiony@icloud.com>
2020-11-17 20:19:06 +0200

    Added: Redux and routes to the App file

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, {Suspense} from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Loader from 'components/Loader/Loader';
+import Routes from 'routes';
+import ErrorBoundary from 'components/ErrorBoundary/ErrorBoundary';
 
 export default function App() {
     return (
-        <div className="App">
-            
-        </div>
+        <ErrorBoundary>
+            <Router>
+                <Suspense fallback={<Loader message="Loading..."/>}>
+                    {Routes}
+                </Suspense>
+            </Router>
+        </ErrorBoundary>
     );
 }
