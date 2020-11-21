bec150cd079a1c44c1f38c482119650ecb8816e3
Adel Bassiony <a.bassiony@icloud.com>
2020-11-21 02:14:38 +0200

    Added: Routes

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,24 @@ import React, { lazy } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 // Lazy Components
-const Error404 = lazy(() => import('./components/Error404/Error404'));
+const Error404 = lazy(() => import('components/Error404/Error404'));
+const Login = lazy(() => import('pages/Login/Login'));
+const Register = lazy(() => import('pages/Register/Register'));
+const Movies = lazy(() => import('pages/Movies/Movies'));
+const WatchList = lazy(() => import('pages/WatchList/WatchList'));
+const ShowMovie = lazy(() => import('pages/ShowMovie/ShowMovie'));
 
 // Application Routes
 const Routes = (
     <Switch>
 
+        <Route path="/" exact component={Movies} />
+        <Route path="/login" exact component={Login} />
+        <Route path="/register" exact component={Register} />
+        <Route path="/watch_list" exact component={WatchList} />
+        <Route path="/movies" exact component={Movies} />
+        <Route path="/movies/:id" exact component={ShowMovie} />
+        
         {/* General Routes */}
         <Route path="*" component={Error404} />
 
